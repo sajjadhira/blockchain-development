cbd2eb3d86df742114e096f05ae467dd0beea413
sajjadhira <sajjad.hira12@gmail.com>
2024-01-18 13:30:47 +0600

    chain validation with test

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -1,3 +1,5 @@
+import pytest
+
 from backend.blockchain.block import Block, GENESIS_DATA
 from backend.blockchain.blockchain import BlockChain
 
@@ -14,3 +16,25 @@ def test_add_block():
     blockchain.add_block(data)
 
     assert blockchain.chain[-1].data == data
+
+
+@pytest.fixture
+def blockchain_three_blocks():
+    blockchain = BlockChain()
+
+    for i in range(3):
+        blockchain.add_block(i)
+
+    return blockchain
+
+
+def test_is_valid_chain(blockchain_three_blocks):
+
+    BlockChain.is_valid_chain(blockchain_three_blocks.chain)
+
+
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
+    blockchain_three_blocks.chain[0].hash = 'evil_hash'
+
+    with pytest.raises(Exception, match='genesis block must be valid'):
+        BlockChain.is_valid_chain(blockchain_three_blocks.chain)
