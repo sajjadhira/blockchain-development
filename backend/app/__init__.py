b3012f778cf04d6a0b249cf5319048e37a63d532
sajjadhira <sajjad.hira12@gmail.com>
2024-01-19 22:54:44 +0600

    Networking has done, block syncronization done.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,7 @@
 import os
+import requests
 import random
+
 from flask import Flask, jsonify
 
 from backend.blockchain.blockchain import BlockChain
@@ -31,10 +33,18 @@ def route_blockchain_mine():
     return jsonify(block.to_json())
 
 
-PORT = 5000
+ROOT_PORT = 5000
+PORT = ROOT_PORT
 
 if os.environ.get('PEER') == 'True':
     PORT = random.randint(5001, 6000)
-
+    result = requests.get(f'http://localhost:{ROOT_PORT}/blockchain')
+    print(result.json())
+    result_blockchain = BlockChain.from_json(result.json())
+    try:
+        blockchain.replace_chain(result_blockchain.chain)
+        print('\n -- Successfully synchronized the local chain')
+    except Exception as e:
+        print(f'\n -- Error synchronizing: {e}')
 
 app.run(port=PORT)
--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -44,6 +44,18 @@ class BlockChain:
         return list(map(lambda block: block.to_json(), self.chain))
 
     @staticmethod
+    def from_json(chain_json):
+        """
+        Deserialize a list of serialized blocks into a BlockChain instance.
+        The result will contain a chain list of Block instances.
+        """
+        blockchain = BlockChain()
+        blockchain.chain = list(
+            map(lambda block_json: Block.from_json(block_json), chain_json))
+
+        return blockchain
+
+    @staticmethod
     def is_valid_chain(chain):
         """
         is_valid_chain: Validate the incoming chain.
