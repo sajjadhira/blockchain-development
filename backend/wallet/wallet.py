be7e01cb7d5a0a69fb71e45bccc2ce871a945e42
sajjadhira <sajjad.hira12@gmail.com>
2024-01-20 12:06:40 +0600

    Singnature added

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,7 +1,9 @@
+import json
 import uuid
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 
 
 class Wallet:
@@ -19,10 +21,25 @@ class Wallet:
         )
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key.
+        """
+        return self.private_key.sign(
+            json.dumps(data).encode('utf-8'),
+            ec.ECDSA(hashes.SHA256())
+        )
+
 
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
+
+    print("\n-------------------\n")
+
+    data = {'foo': 'bar'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
 
 
 if __name__ == '__main__':
